8bda078cb4ce1f3177e434f63d284503dfe35bd1
Jonathon Toon <contact@jonathontoon.com>
2016-06-25 01:30:50 +0900

    updated chart

--- MarketTrak/MTItemPriceHistoryViewController.swift
+++ MarketTrak/MTItemPriceHistoryViewController.swift
@@ -99,41 +99,73 @@ class MTItemPriceHistoryViewController: MTModalViewController {
         containerTitleView.addSubview(subTitleLabel)
 
         
-        sortPricesByDateRange(.Lifetime)
+        sortPricesByDateRange(.Week)
         
         view.addSubview(graphView)
-        graphView.backgroundColor = UIColor.appTintColor()
-        graphView.backgroundFillColor = UIColor.backgroundColor()
-        graphView.lineColor = UIColor.appTintColor().colorWithAlphaComponent(0.5)
-        graphView.barLineColor = UIColor.appTintColor()
-        graphView.dataPointFillColor = UIColor.appTintColor()
-        graphView.fillColor = UIColor.appTintColor().colorWithAlphaComponent(0.2)
+//        graphView.backgroundColor = UIColor.appTintColor()
+//        graphView.backgroundFillColor = UIColor.backgroundColor()
+//        graphView.lineColor = UIColor.appTintColor().colorWithAlphaComponent(0.5)
+//        graphView.barLineColor = UIColor.appTintColor()
+//        graphView.dataPointFillColor = UIColor.appTintColor()
+//        graphView.fillColor = UIColor.appTintColor().colorWithAlphaComponent(0.2)
+//        graphView.shouldFill = true
+//        graphView.dataPointLabelFont = UIFont.systemFontOfSize(10, weight: UIFontWeightRegular)
+//        graphView.dataPointLabelColor = UIColor.whiteColor()
+//        graphView.dataPointLabelTopMargin = 30
+//        graphView.topMargin = 20
+//        graphView.leftmostPointPadding = 30
+//        graphView.bottomMargin = 20
+//        graphView.shouldDrawDataPoint = true
+//        graphView.dataPointSize = 4
+//        graphView.referenceLineThickness = 1.0/UIScreen.mainScreen().scale
+//        graphView.referenceLineColor = UIColor.whiteColor().colorWithAlphaComponent(0.12)
+//        graphView.referenceLineLabelFont = UIFont.systemFontOfSize(10, weight: UIFontWeightRegular)
+//        graphView.referenceLineLabelColor = UIColor.whiteColor()
+//        graphView.shouldAnimateOnStartup = false
+//        graphView.shouldAdaptRange = false
+//        graphView.shouldAnimateOnAdapt = false
+//        graphView.numberOfIntermediateReferenceLines = 5
+//        graphView.referenceLineNumberOfDecimalPlaces = 2
+//        graphView.referenceLineUnits = "USD"
+//        graphView.shouldAddUnitsToIntermediateReferenceLineLabels = true
+//        graphView.shouldAutomaticallyDetectRange = true
+//        //graphView.dataPointSpacing = 54
+//        //graphView.direction = .RightToLeft
+//        graphView.showsHorizontalScrollIndicator = false
+
+        graphView.backgroundFillColor = UIColor(rgba: "#333333")
+        
+        graphView.lineWidth = 1
+        graphView.lineColor = UIColor(rgba: "#777777")
+        graphView.lineStyle = ScrollableGraphViewLineStyle.Smooth
+        
         graphView.shouldFill = true
-        graphView.dataPointLabelFont = UIFont.systemFontOfSize(10, weight: UIFontWeightRegular)
-        graphView.dataPointLabelColor = UIColor.whiteColor()
-        graphView.dataPointLabelTopMargin = 30
+        graphView.fillType = ScrollableGraphViewFillType.Gradient
+        graphView.fillColor = UIColor(rgba: "#555555")
+        graphView.fillGradientType = ScrollableGraphViewGradientType.Linear
+        graphView.fillGradientStartColor = UIColor(rgba: "#555555")
+        graphView.fillGradientEndColor = UIColor(rgba: "#444444")
+        
+        graphView.dataPointSpacing = 48
+        graphView.dataPointSize = 2
+        graphView.dataPointFillColor = UIColor.whiteColor()
         graphView.topMargin = 20
-        graphView.leftmostPointPadding = 0
+        graphView.leftmostPointPadding = 43
         graphView.rightmostPointPadding = 0
-        graphView.bottomMargin = 20
-        graphView.shouldDrawDataPoint = true
-        graphView.dataPointSize = 4
-        graphView.referenceLineThickness = 1.0/UIScreen.mainScreen().scale
-        graphView.referenceLineColor = UIColor.whiteColor().colorWithAlphaComponent(0.12)
-        graphView.referenceLineLabelFont = UIFont.systemFontOfSize(10, weight: UIFontWeightRegular)
+        
+        graphView.referenceLineLabelFont = UIFont.boldSystemFontOfSize(8)
+        graphView.referenceLineColor = UIColor.whiteColor().colorWithAlphaComponent(0.2)
         graphView.referenceLineLabelColor = UIColor.whiteColor()
-        graphView.shouldAnimateOnStartup = false
-        graphView.shouldAdaptRange = false
-        graphView.shouldAnimateOnAdapt = true
+        graphView.dataPointLabelColor = UIColor.whiteColor().colorWithAlphaComponent(0.5)
         graphView.numberOfIntermediateReferenceLines = 5
         graphView.referenceLineNumberOfDecimalPlaces = 2
         graphView.referenceLineUnits = "USD"
         graphView.shouldAddUnitsToIntermediateReferenceLineLabels = true
-        graphView.shouldRangeAlwaysStartAtZero = true
         graphView.shouldAutomaticallyDetectRange = true
-        graphView.dataPointSpacing = 50
-        graphView.direction = .RightToLeft
-        graphView.showsHorizontalScrollIndicator = false
+        graphView.shouldAnimateOnAdapt = false
+        graphView.shouldAnimateOnStartup = false
+        graphView.shouldAdaptRange = true
+        
         graphView.autoPinEdge(.Top, toEdge: .Top, ofView: view, withOffset: 45)
         graphView.autoPinEdge(.Left, toEdge: .Left, ofView: view, withOffset: -2)
         graphView.autoPinEdge(.Right, toEdge: .Right, ofView: view, withOffset: 2)
@@ -141,7 +173,7 @@ class MTItemPriceHistoryViewController: MTModalViewController {
         
         view.addSubview(dateSegmentedControl)
         dateSegmentedControl.tintColor = UIColor.appTintColor()
-        dateSegmentedControl.selectedSegmentIndex = 0
+        dateSegmentedControl.selectedSegmentIndex = 2
         dateSegmentedControl.addTarget(self, action: #selector(MTItemPriceHistoryViewController.segmentSelected(_:)), forControlEvents: .ValueChanged)
         dateSegmentedControl.autoPinEdge(.Top, toEdge: .Top, ofView: view, withOffset: 15)
         dateSegmentedControl.autoPinEdge(.Left, toEdge: .Left, ofView: view, withOffset: 15)
@@ -174,7 +206,8 @@ class MTItemPriceHistoryViewController: MTModalViewController {
         price = []
         labels = []
         
-        var previousDate: NSDate!
+        var prices: [Double] = []
+        var dates: [NSDate] = []
         
         if let priceHistoryArray = item.priceHistory {
             for priceHistoryItem in priceHistoryArray {
@@ -184,21 +217,26 @@ class MTItemPriceHistoryViewController: MTModalViewController {
                 
                 if let offset = offsetDate {
                     if dateValue.compare(offset) == NSComparisonResult.OrderedDescending || dateValue.compare(offset) == NSComparisonResult.OrderedSame {
-                        
-                        if previousDate != nil {
-                            if !NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)!.isDate(previousDate, inSameDayAsDate: dateValue) {
-                                addPriceValue(value: priceValue)
-                                addDateAsLabelValue(dateValue)
-                            }
-                        } else {
-                            addPriceValue(value: priceValue)
-                            addDateAsLabelValue(dateValue)
-                        }
-                        
-                        previousDate = dateValue
+                        prices.append(priceValue)
+                        dates.append(dateValue)
                     }
                 }
             }
+            
+            addPriceValue(value: prices[0])
+            addPriceValue(value: prices[Int(Double(prices.count)*0.16)])
+            addPriceValue(value: prices[Int(Double(prices.count)*0.32)])
+            addPriceValue(value: prices[Int(Double(prices.count)*0.48)])
+            addPriceValue(value: prices[Int(Double(prices.count)*0.64)])
+            addPriceValue(value: prices[Int(Double(prices.count)*0.8)])
+            addPriceValue(value: prices[prices.count-1])
+            addDateAsLabelValue(dates[0])
+            addDateAsLabelValue(dates[Int(Double(dates.count)*0.16)])
+            addDateAsLabelValue(dates[Int(Double(dates.count)*0.32)])
+            addDateAsLabelValue(dates[Int(Double(dates.count)*0.48)])
+            addDateAsLabelValue(dates[Int(Double(dates.count)*0.64)])
+            addDateAsLabelValue(dates[Int(Double(dates.count)*0.8)])
+            addDateAsLabelValue(dates[dates.count-1])
         }
         
         graphView.setData(price, withLabels: labels)
@@ -233,10 +271,6 @@ class MTItemPriceHistoryViewController: MTModalViewController {
             dateFormatter.dateFormat = "MMM dd"
             dateFormatter.calendar = NSCalendar(calendarIdentifier: NSCalendarIdentifierGregorian)
         
-        if dateValue == item.priceHistory![0].date || dateValue == item.priceHistory![item.priceHistory!.count-1].date  {
-            labels.append("")
-        } else {
-            labels.append(dateFormatter.stringFromDate(dateValue).uppercaseString)
-        }
+        labels.append(dateFormatter.stringFromDate(dateValue).uppercaseString)
     }
 }
